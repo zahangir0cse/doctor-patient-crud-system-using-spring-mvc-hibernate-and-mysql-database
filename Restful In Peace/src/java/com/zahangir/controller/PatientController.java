08ab7e886271e7cb74ba425bb323ff3631946c80
Zahangir Alam <zahangir0cse@gmail.com>
2017-07-10 05:23:58 +0600

    changed dao, service and controller

---
+++ Restful In Peace/src/java/com/zahangir/controller/PatientController.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.zahangir.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ *
+ * @author Zahangir Alam
+ */
+@Controller
+public class PatientController {
+    
+}
