89bb8d85da3bfa58d4e2e08d80fffc8c8cb71879
Zahangir Alam <zahangir0cse@gmail.com>
2017-07-10 13:47:43 +0600

    add patient in controller

--- Restful In Peace/src/java/com/zahangir/controller/PatientController.java
+++ Restful In Peace/src/java/com/zahangir/controller/PatientController.java
@@ -1,17 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.zahangir.controller;
 
+import com.zahangir.model.Patient;
+import com.zahangir.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- *
- * @author Zahangir Alam
- */
 @Controller
 public class PatientController {
     
+    @Autowired
+    PatientService patientService;
+    
+    @RequestMapping(value = "/patient/add", method = RequestMethod.POST)
+    public String addPatient(@ModelAttribute("patient") Patient patient, BindingResult result){
+        if (patient.getPatientId() == null) {
+            patientService.addPatient(patient);
+        }
+        return "";
+        
+    }
 }
---
+++ Restful In Peace/web/WEB-INF/jsp/addpatient.jsp
@@ -0,0 +1,20 @@
+<%-- 
+    Document   : addpatient
+    Created on : Jul 10, 2017, 1:07:24 PM
+    Author     : Zahangir Alam
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@taglib uri="http://www.springframework.org/tags" prefix="spring" %>
+<%@taglib uri="http://www.springframework.org/tags/form" prefix="form" %>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Patient info</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
